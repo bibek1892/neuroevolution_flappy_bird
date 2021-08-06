fcc80c4062eeee13ba133a7cc3112be488c0fd9a
bibekkhatiwada16 <bibekkhatiwada16@gmail.com>
2021-08-06 23:16:35 +0545

    initial bird to populations..

--- pipe.js
+++ pipe.js
@@ -1,57 +1,58 @@
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
 
-// creating a pipe object with a constructor function
+// This flappy bird implementation is adapted from:
+// https://youtu.be/cXgA1d_E-jY&
 
-function Pipe(){
+class Pipe {
+  constructor() {
 
-    this.top = random(height/2);  // value from top to middle of the screen
-    this.bottom = random(height/2); // middle of screen to the bottom
-    this.x=width;  // start at the edge of the screen
-    this.w=20;
-    this.speed = 2;
+    // How big is the empty space
+    let spacing = 125;
+    // Where is th center of the empty space
+    let centery = random(spacing, height - spacing);
 
-    this.highlight =  false;
+    // Top and bottom of pipe
+    this.top = centery - spacing / 2;
+    this.bottom = height - (centery + spacing / 2);
+    // Starts at the edge
+    this.x = width;
+    // Width of pipe
+    this.w = 80;
+    // How fast
+    this.speed = 6;
+  }
 
-    this.hits = function(bird){
-
-        if(bird.y < this.top || bird.y > height - this.bottom ){
-            if(bird.x > this.x && bird.x < this.x + this.w){
-                this.highlight =  true;
-                return true;
-            }
-             
-        }
-        this.highlight = false;
-        return false;
-
+  // Did this pipe hit a bird?
+  hits(bird) {
+    if ((bird.y - bird.r) < this.top || (bird.y + bird.r) > (height - this.bottom)) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        return true;
+      }
     }
+    return false;
+  }
 
+  // Draw the pipe
+  show() {
+    stroke(255);
+    fill(200);
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  }
 
-    this.show = function(){
-        fill(255);
-        if(this.highlight){
-            fill(255,0,0);      //making red if collide
-        }
-        rect(this.x, 0, this.w, this.top);
-        rect(this.x, height-this.bottom, this.w, this.bottom);
+  // Update the pipe
+  update() {
+    this.x -= this.speed;
+  }
 
+  // Has it moved offscreen?
+  offscreen() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
     }
-
-    this.update = function(){
-
-        this.x -= this.speed;
-
-    }
-
-    this.offscreen = function(){
-
-        if(this.x < -this.w){
-            return true;
-        }
-        else{
-            return false;
-        }
-
-
-    }
-
+  }
 }
